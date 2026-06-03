c12eb5eec4b4f59ace892693009686e0065c9fd9
Alexander <59964601+AlexanderRU44@users.noreply.github.com>
2026-06-03 09:41:23 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs, where } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
+import { getDatabase, ref, set, get, onValue, remove, update, push } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCiTAsiUVnWwT4DfIi70wvYwuVJnYqoK20",
@@ -9,10 +9,10 @@ const firebaseConfig = {
     storageBucket: "retro-snake-94402.firebasestorage.app",
     messagingSenderId: "176962387007",
     appId: "1:176962387007:web:15abe95b45d9f6f9a54c8a",
-    databaseURL: "https://retro-snake-94402-default-rtdb.firebaseio.com/"
+    databaseURL: "https://retro-snake-94402-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
-export { collection, addDoc, query, orderBy, limit, getDocs, where };
+export { collection, addDoc, query, orderBy, limit, getDocs, where, ref, set, get, onValue, remove, update, push };
